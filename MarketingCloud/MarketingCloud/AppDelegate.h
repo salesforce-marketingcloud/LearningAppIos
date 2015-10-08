a1e54478204623e7b2a959151c6ebb3bb6041e3b
Mathias Moller <mathias.moller.01@gmail.com>
2015-10-08 15:45:31 -0200

    new project

---
+++ MarketingCloud/MarketingCloud/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarketingCloud
+//
+//  Created by Mathias on 10/8/15.
+//  Copyright © 2015 Oktana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MarketingCloud/MarketingCloud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MarketingCloud
+//
+//  Created by Mathias on 10/8/15.
+//  Copyright © 2015 Oktana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

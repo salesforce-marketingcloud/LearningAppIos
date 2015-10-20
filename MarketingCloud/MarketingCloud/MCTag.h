32e47e3fa6cab5967e4df305ec2cd9f31aac7964
Mathias Moller <mathias.moller.01@gmail.com>
2015-10-20 13:39:16 -0200

    modify tag object

--- MarketingCloud/MarketingCloud/MCTag.h
+++ MarketingCloud/MarketingCloud/MCTag.h
@@ -3,11 +3,13 @@
 //  MarketingCloud
 //
 //  Created by Mathias on 10/20/15.
-//  Copyright © 2015 Oktana. All rights reserved.
+//  Copyright © 2015 Salesforce Marketing Cloud. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface MCTag : NSObject
+@property(nonatomic, strong) NSString * name;
+@property(nonatomic, assign) BOOL on;
 
 @end

ea95fad16eb8272f6187e640e5e61d6cb6568e6b
Mathias Moller <mathias.moller.01@gmail.com>
2015-10-20 13:39:50 -0200

    modify view and cell for table view controller

--- MarketingCloud/MarketingCloud/MCTagTableCell.h
+++ MarketingCloud/MarketingCloud/MCTagTableCell.h
@@ -3,11 +3,43 @@
 //  MarketingCloud
 //
 //  Created by Mathias on 10/19/15.
-//  Copyright © 2015 Oktana. All rights reserved.
+//  Copyright © 2015 Salesforce Marketing Cloud. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface MCTagTableCell : NSObject
+@class MCTag;
+
+@protocol MCTagTableCellDelegate <NSObject>
+
+- (void) notificationRecive:(BOOL)recive indexPath:(NSIndexPath*)indexPath;
+
+@end
+
+
+@interface MCTagTableCell : UITableViewCell
+
+/**
+ Delegate for the cell
+ */
+@property (nonatomic, weak) id<MCTagTableCellDelegate> delegate;
+
+/**
+ Displays title of current setting
+ */
+@property (nonatomic, strong) IBOutlet UILabel *titleLabel;
+
+/**
+ Allows user to turn notifications on or off
+ */
+@property (nonatomic, strong) IBOutlet UISwitch *notificationSwitch;
+
+/**
+ Sets up the cell view using the tag present in the MCTag object
+ 
+ @param tag The MCTag containing the data that should be displayed in the cell.
+ @param indexPath The indexPath of the cell that is being configured.
+ */
+- (void)configureWithTag:(MCTag *)tag indexPath:(NSIndexPath *)indexPath;
 
 @end
--- MarketingCloud/MarketingCloud/MCTagsViewController.m
+++ MarketingCloud/MarketingCloud/MCTagsViewController.m
@@ -7,11 +7,29 @@
 //
 
 #import "MCTagsViewController.h"
+
+// Libraries
 #import "ETPush.h"
 
-@interface MCTagsViewController ()
+// Models
+#import "MCTag.h"
 
-@property(nonatomic, strong) NSArray *tags;
+// Table cell
+#import "MCTagTableCell.h"
+
+static NSString *cellIdentifier = @"MCTagTableCell";
+
+@interface MCTagsViewController () <MCTagTableCellDelegate>
+
+/**
+ Array all tag object
+ */
+@property(nonatomic, strong) NSMutableArray *tags;
+
+/**
+ Text field. name nuew tag
+ */
+@property (weak, nonatomic) IBOutlet UITextField *tagName;
 
 @end
 
@@ -22,8 +40,19 @@
     
     // pushManager returns the list of tags as a NSSet collection
     NSSet *setOfTags = [[ETPush pushManager] allTags];
-    // we turn that NSSet into a NSMutableArray in order to make it manipulable
-    self.tags = [NSMutableArray arrayWithArray:[setOfTags allObjects]];
+    
+    // init mutable array
+    self.tags = [[NSMutableArray alloc]initWithCapacity:setOfTags.count];
+    
+    
+    MCTag *tag;
+    
+    // created object MCTag and add in tags array
+    for (NSString* nameTag in [setOfTags allObjects]) {
+        tag = [[MCTag alloc] init];
+        tag.name = nameTag;
+        [self.tags addObject:tag];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -32,38 +61,69 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return [self.tags count];
 }
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
 
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 40;
 }
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    MCTagTableCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    [cell configureWithTag:self.tags[indexPath.row] indexPath:indexPath];
+    cell.delegate = self;
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+#pragma mark - tag cell view  delegate
+
+- (void) notificationRecive:(BOOL)recive indexPath:(NSIndexPath*)indexPath {
+    MCTag *tag  = self.tags[indexPath.row];
+    tag.on      = recive;
     
-    static NSString *cellIdentifier = @"tagCel";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    /**
+      Add or remove tags for the current device.
+     */
+    if (recive) {
+        [[ETPush pushManager] addTag:tag.name];
+    } else {
+        [[ETPush pushManager] removeTag:tag.name];
     }
-    NSString *tag = (NSString *)[self.tags objectAtIndex:indexPath.row];
-    cell.textLabel.text = tag;
-    return cell;
+    
 }
 
-
+- (IBAction)newTag:(id)sender {
+    /**
+     Tags don't have to be defined inside of MobilePush before using them. You can create them at will
+     Create new tag
+     */
+    MCTag *tag = [[MCTag alloc]init];
+    tag.name = self.tagName.text;
+    tag.on = YES;
+    
+    // add tag in tags array
+    [self.tags addObject:tag];
+    
+    // add tafs for the current device.
+    [[ETPush pushManager] addTag:tag.name];
+    
+    self.tagName.text  = @"";
+    
+    // reload table
+    [self.tableView reloadData];
+}
 
 @end

5e9f14882ba7e57319afe960cd235760201f5347
Mathias Moller <mathias.moller.01@gmail.com>
2015-10-23 17:44:35 -0300

    add maps and annotation

--- MarketingCloud/MarketingCloud/MCSubscribeKeyViewController.m
+++ MarketingCloud/MarketingCloud/MCSubscribeKeyViewController.m
@@ -6,24 +6,25 @@
 //  Copyright © 2015 Oktana. All rights reserved.
 //
 
-#import "MCHomeView.h"
+#import "MCSubscribeKeyViewController.h"
 
 #import "ETPush.h" // From the SDK
 
-@interface MCHomeView ()
+@interface MCSubscribeKeyViewController () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *subscriberKey;
 
 @end
 
-@implementation MCHomeView
+@implementation MCSubscribeKeyViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-    // The getSubscriberKey() method returns the value
-    // The getSubscriberKey() method retrieves only the local copy of the value and not the value from the Salesforce Marketing Cloud.
-    self.subscriberKey.text = [[ETPush pushManager] getSubscriberKey];
+    /**
+     The getSubscriberKey() method returns the value
+     The getSubscriberKey() method retrieves only the local copy of the value and not the value from the Salesforce Marketing Cloud.
+     */
+     self.subscriberKey.text = [[ETPush pushManager] getSubscriberKey];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,16 +32,25 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)saveSubscriberKey:(id)sender {
-    
-    // Use setSubscriberKey() to implement a specific value as the unique identifier for the contact.
-    // You can use a mobile number, email address, device ID, or other appropriate value as necessary.
+    /**
+     Use setSubscriberKey() to implement a specific value as the unique identifier for the contact.
+     You can use a mobile number, email address, device ID, or other appropriate value as necessary.
+     */
     [[ETPush pushManager] setSubscriberKey:self.subscriberKey.text];
 }
 
 - (IBAction)reloadSubscriberKey:(id)sender {
-    // The getSubscriberKey() method returns the value
-    // The getSubscriberKey() method retrieves only the local copy of the value and not the value from the Salesforce Marketing Cloud.
+    /**
+     The getSubscriberKey() method returns the value
+     The getSubscriberKey() method retrieves only the local copy of the value and not the value from the Salesforce Marketing Cloud.
+    */
     self.subscriberKey.text = [[ETPush pushManager] getSubscriberKey];
+}
+
+#pragma mark - <UITextFieldDelegate>
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return NO;
 }
 
 @end

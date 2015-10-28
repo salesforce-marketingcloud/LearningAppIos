b60b8cc96b9ef76a4fd7e5ccf4c1c1ee4fb7be38
Mathias Moller <mathias.moller.01@gmail.com>
2015-10-28 09:13:37 -0300

    add new annotation and circule

---
+++ MarketingCloud/MarketingCloud/MCCircle.h
@@ -0,0 +1,13 @@
+//
+//  MCCircle.h
+//  MarketingCloud
+//
+//  Created by Mathias on 10/28/15.
+//  Copyright © 2015 Oktana. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface MCCircle : MKCircle
+
+@end
---
+++ MarketingCloud/MarketingCloud/MCCircle.m
@@ -0,0 +1,13 @@
+//
+//  MCCircle.m
+//  MarketingCloud
+//
+//  Created by Mathias on 10/28/15.
+//  Copyright © 2015 Oktana. All rights reserved.
+//
+
+#import "MCCircle.h"
+
+@implementation MCCircle
+
+@end

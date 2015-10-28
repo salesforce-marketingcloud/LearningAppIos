45693815d5d912a0b99b3558c858c0316f12ad96
Mathias Moller <mathias.moller.01@gmail.com>
2015-10-28 16:55:59 -0300

    add view and view controller inbox and add images for the tabs

---
+++ MarketingCloud/MarketingCloud/MCInboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  MCInboxViewController.h
+//  MarketingCloud
+//
+//  Created by Mathias on 10/28/15.
+//  Copyright © 2015 Salesforce Marketing Cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCInboxViewController : UIViewController
+
+@end

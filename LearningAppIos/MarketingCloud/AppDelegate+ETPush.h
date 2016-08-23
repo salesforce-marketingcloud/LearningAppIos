ff42b8f1d4a14fdb8118660557812a81cac5b393
Brian M. Criscuolo <bcriscuolo@salesforce.com>
2016-08-23 10:23:57 -0400

    - support iOS 10 notifications in learning app

--- LearningAppIos/MarketingCloud/AppDelegate+ETPush.h
+++ LearningAppIos/MarketingCloud/AppDelegate+ETPush.h
@@ -6,17 +6,9 @@
  */
 
 #import "AppDelegate.h"
-
-#if __IPHONE_OS_VERSION_MAX_ALLOWED > __IPHONE_9_3
 #import <UserNotifications/UserNotifications.h>
 
 @interface AppDelegate (ETPush) <UNUserNotificationCenterDelegate, ExactTargetCloudPageWithAlertDelegate>
-
-#else
-
-@interface AppDelegate (ETPush) <ExactTargetCloudPageWithAlertDelegate>
-
-#endif
 
 - (BOOL)application : (UIApplication *)application shouldInitETSDKWithOptions : (NSDictionary *)launchOptions;
 

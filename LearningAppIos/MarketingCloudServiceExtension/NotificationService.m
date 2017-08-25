fc6fabb2d98ecfc27d801f181b54b490a15a5692
Brian M. Criscuolo <bcriscuolo@salesforce.com>
2017-08-25 11:33:37 -0400

    - added service extension for 210

--- LearningAppIos/MarketingCloudServiceExtension/NotificationService.m
+++ LearningAppIos/MarketingCloudServiceExtension/NotificationService.m
@@ -11,72 +11,133 @@
 @interface NotificationService ()
 
 @property (nonatomic, strong) void (^contentHandler)(UNNotificationContent *contentToDeliver);
-@property (nonatomic, strong) UNMutableNotificationContent *bestAttemptContent;
+@property (nonatomic, strong) UNMutableNotificationContent *modifiedNotificationContent;
 
 @end
 
-/*
- 
- Imagine sending this payload:
- 
- {
- "aps": {
- "alert": {
- "title": "BAM",
- "body": "look at that!"
- },
- "badge": 1,
- "mutable-content": 1
- },
- "imageURL":"https://i.giphy.com/o9ggk5IMcYlKE.gif"
- }
- 
- This service extension will download the file at imageURL locally and present it as the notification content
- */
-
 @implementation NotificationService
 
-- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
-    self.contentHandler = contentHandler;
-    self.bestAttemptContent = [request.content mutableCopy];
-    
-    self.bestAttemptContent.title = @"BAM!";
+- (UNNotificationAttachment *)createMediaAttachment:(NSURL *)localMediaUrl {
+    // options: specify what cropping rectangle of the media to use for a thumbnail
+    //          whether the thumbnail is hidden or not
+    //          which frame to use for the thumbnail if the media is a movie
+    UNNotificationAttachment *mediaAttachment = [UNNotificationAttachment attachmentWithIdentifier: @"attachmentIdentifier"
+                                                                                               URL: localMediaUrl
+                                                                                           options: @{
+                                                                                                      UNNotificationAttachmentOptionsThumbnailClippingRectKey: (NSDictionary *)CFBridgingRelease(CGRectCreateDictionaryRepresentation(CGRectZero)),
+                                                                                                      UNNotificationAttachmentOptionsThumbnailHiddenKey: @NO,
+                                                                                                      UNNotificationAttachmentOptionsThumbnailTimeKey: @(1)}
+                                                                                             error: nil];
+    return mediaAttachment;
+}
 
-    NSString *imageURL = request.content.userInfo[@"imageURL"];
-    if (imageURL != nil) {
+- (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+    
+    // save the completion handler we will call back later
+    self.contentHandler = contentHandler;
+    
+    // make a copy of the notification so we can change it
+    self.modifiedNotificationContent = [request.content mutableCopy];
+    
+    // alternative text to display if there are any issues loading the media URL
+    NSString *mediaAltText = request.content.userInfo[@"_mediaAlt"];
+    
+    // does the payload contains a remote URL to download or a local URL?
+    NSString *mediaUrlString = request.content.userInfo[@"_mediaUrl"];
+    NSURL *mediaUrl = [NSURL URLWithString: mediaUrlString];
+    
+    // if we have a URL, try to download media (i.e., https://media.giphy.com/media/3oz8xJBbCpzG9byZmU/giphy.gif)
+    if (mediaUrl != nil) {
+        // create a session to handle downloading of the URL
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
         
-        NSURLSession *session = [NSURLSession sharedSession];
-        [[session dataTaskWithURL:[NSURL URLWithString:imageURL]
-                completionHandler:^(NSData *data,
-                                    NSURLResponse *response,
-                                    NSError *error) {
+        // start a download task to handle the download of the media
+        __weak __typeof__(self) weakSelf = self;
+        [[session downloadTaskWithURL:mediaUrl completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            BOOL useAlternateText = YES;
+            
+            // if the download succeeded, save it locally and then make an attachment
+            if (error == nil) {
+                if (200 <= ((NSHTTPURLResponse*)response).statusCode  && ((NSHTTPURLResponse*)response).statusCode <= 299) {
+                    // download was successful, attempt save the media file
+                    NSURL *localMediaUrl = [NSURL fileURLWithPath:[location.path stringByAppendingString:mediaUrl.lastPathComponent]];
                     
-                    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-                    NSString *documentsPath = [paths objectAtIndex:0];
-                    NSString *filePath = [documentsPath stringByAppendingPathComponent:@"image.gif"];
-                    BOOL writeSuccess = [data writeToFile:filePath atomically:YES];
-                    if (writeSuccess == YES) {
-                        NSURL *fileURL = [NSURL fileURLWithPath:filePath];
+                    // remove any existing file with the same name
+                    [[NSFileManager defaultManager] removeItemAtURL:localMediaUrl error:nil];
+                    
+                    // move the downloaded file from the temporary location to a new file
+                    if ([[NSFileManager defaultManager] moveItemAtURL:location toURL:localMediaUrl error:nil] == YES) {
+                        // create an attachment with the new file
+                        UNNotificationAttachment * mediaAttachment = [weakSelf createMediaAttachment:localMediaUrl];
                         
-                        NSError *error2 = nil;
-                        NSDictionary *thumbSize = (NSDictionary *)CFBridgingRelease(CGRectCreateDictionaryRepresentation(CGRectMake(0, 0, 1, 1)));
-                        UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:@"" URL:fileURL options:@{UNNotificationAttachmentOptionsThumbnailClippingRectKey: thumbSize} error:&error2];
-                        self.bestAttemptContent.attachments = @[attachment];
-                        
-                        self.contentHandler(self.bestAttemptContent);
+                        // if no problems creating the attachment, we can use it
+                        if (mediaAttachment != nil) {
+                            // set the media to display in the notification
+                            weakSelf.modifiedNotificationContent.attachments = @[mediaAttachment];
+                            
+                            // everything is ok
+                            useAlternateText = NO;
+                        }
                     }
-                    
-                }] resume];
+                }
+            }
+            
+            // if any problems creating the attachment, use the alternate text if provided
+            if ((useAlternateText == YES) && (mediaAltText != nil)) {
+                weakSelf.modifiedNotificationContent.body = mediaAltText;
+            }
+            
+            // tell the OS we are done and here is the new content
+            weakSelf.contentHandler(weakSelf.modifiedNotificationContent);
+        }] resume];
     }
     else {
-        self.contentHandler(self.bestAttemptContent);
+        // see if the media URL is for a local file  (i.e., file://movie.mp4)
+        BOOL useAlternateText = YES;
+        if (mediaUrlString != nil) {
+            // attempt to create a URL to a file in local storage
+            NSURL *localMediaUrl = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource: mediaUrlString.lastPathComponent.stringByDeletingLastPathComponent ofType:mediaUrlString.pathExtension]];
+            
+            // is the URL a local file URL?
+            if (localMediaUrl != nil && localMediaUrl.isFileURL == YES) {
+                // create an attachment with the local media
+                UNNotificationAttachment * mediaAttachment = [self createMediaAttachment:localMediaUrl];
+                
+                // if no problems creating the attachment, we can use it
+                if (mediaAttachment != nil) {
+                    // set the media to display in the notification
+                    self.modifiedNotificationContent.attachments = @[mediaAttachment];
+                    
+                    // everything is ok
+                    useAlternateText = NO;
+                }
+            }
+        }
+        
+        // if any problems creating the attachment, use the alternate text if provided
+        if ((useAlternateText == YES) && (mediaAltText != nil)) {
+            self.modifiedNotificationContent.body = mediaAltText;
+        }
+        
+        // tell the OS we are done and here is the new content
+        contentHandler(self.modifiedNotificationContent);
     }
 }
 
 - (void)serviceExtensionTimeWillExpire {
     // Called just before the extension will be terminated by the system.
     // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-    self.contentHandler(self.bestAttemptContent);
+    
+    // we took too long to download the media URL, use the alternate text if provided
+    NSString *mediaAltText = self.modifiedNotificationContent.userInfo[@"_mediaAlt"];
+    if (mediaAltText != nil) {
+        self.modifiedNotificationContent.body = mediaAltText;
+    }
+    
+    // tell the OS we are done and here is the new content
+    self.contentHandler(self.modifiedNotificationContent);
 }
 
 @end
+
+
